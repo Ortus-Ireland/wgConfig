3032163580d04ae6ec7546a6a8fedd0203b05606
Declan Dunne <ddunne@ortus.ie>
2021-04-07 11:54:50 +0100

    Updated iptables persistance

--- wg.sh
+++ wg.sh
@@ -4,8 +4,8 @@ sudo apt-get update -y
 sudo apt install wireguard -y
 
 
-mkdir wg &&
-mkdir wg/keys &&
+mkdir ./home/ortusadmin/wg &&
+mkdir ./home/ortusadmin/wg/keys &&
 umask 077 &&
 wg genkey |tee wg/keys/server_private_key|wg pubkey>wg/keys/server_public_key
 
@@ -17,7 +17,6 @@ ListenPort = 443
 PrivateKey=$(cat wg/keys/server_private_key)"|sudo tee /etc/wireguard/wg0.conf
 
 sudo sysctl -w net.ipv4.ip_forward=1
-
 
 # Assign the filename
 filename="/etc/sysctl.conf"
@@ -32,7 +31,6 @@ if [[ $search != "" && $replace != "" ]]; then
 sed -i "s/$search/$replace/" $filename
 fi
 
-
 sudo sysctl -p /etc/sysctl.conf
 
 sudo iptables -A FORWARD -i wg0 -j ACCEPT
@@ -41,9 +39,12 @@ sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
 sudo iptables -t nat -A POSTROUTING -s 10.200.200.0/24 -o eth0 -j MASQUERADE
 
-sudo apt install iptables-persistent &&
-sudo systemctl enable netfilter-persistent &&
-sudo netfilter-persistent save
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
 
+sudo apt install iptables-persistent -y
+sudo systemctl enable netfilter-persistent
+sudo netfilter-persistent save
+ 
 sudo wg-quick up wg0 &&log
 sudo systemctl enable wg-quick@wg0
