294b17ea196cdec77b8ea02059efffcdd10e0330
Declan Dunne <81355373+RealDeclan@users.noreply.github.com>
2021-05-12 16:10:11 +0100

    Added env var to pass username
    Added env variable to pass username to script to adapt directory to main user

--- wg-backup.sh
+++ wg-backup.sh
@@ -1,36 +1,6 @@
 #!/bin/bash
+SrvUser=$1
 BACKUPTIME=`date +%b-%d-%y`
+dest=/home/${SrvUser}/wg/backups/w0conf-${BACKUPTIME}.tar
+sudo tar -cf $dest /etc/wireguard/wg0.conf /home/${SrvUser}/wg/clients /home/${SrvUser}/wg/key
 
-
-
-
-
-# Uncomment me after testing and add to the bottom of the deployment script
-# dest=/home/${SrvUser}/wg/backups/w0conf-${BACKUPTIME}.tar
-# sudo tar -cf $dest /etc/wireguard/wg0.conf /home/${SrvUser}/wg/clients /home/${SrvUser}/wg/key
-
-# dest=/home/${SrvUser}/wg/backups/w0conf-${BACKUPTIME}.tar
-# sudo tar -cf $dest /etc/wireguard/wg0.conf /home/${SrvUser}/wg/clients /home/${SrvUser}/wg/key
-
-# crontab -l > wgcron
-# #echo new cron into cron file
-# echo "* * * * * /home/${SrvUser}/wg/backup/backup.sh" >> wgcron
-# #install new cron file
-# crontab wgcron
-# rm wgcron
-
-
-# TESTING SCRIPT
-
-dest=/home/ortusadmin/wg/backup/w0conf-${BACKUPTIME}.tar
-sudo tar -cf $dest /etc/wireguard/wg0.conf /home/ortusadmin/wg/clients /home/ortusadmin/wg/key
-
-dest=/home/ortusadmin/wg/backups/w0conf-${BACKUPTIME}.tar
-sudo tar -cf $dest /etc/wireguard/wg0.conf /home/ortusadmin/wg/clients /home/ortusadmin/wg/key
-
-crontab -l > wgcron
-#echo new cron into cron file
-echo "* * * * * /home/ortusadmin/wg/backup/backup.sh" >> wgcron
-#install new cron file
-crontab wgcron
-rm wgcron
