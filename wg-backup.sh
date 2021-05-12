2f2df7a5ed571623c1e2f17bf1b3317f7df0f618
Declan Dunne <81355373+RealDeclan@users.noreply.github.com>
2021-05-12 15:29:18 +0100

    Testing Deployment prior to merge

--- wg-backup.sh
+++ wg-backup.sh
@@ -1,4 +1,36 @@
 #!/bin/bash
 BACKUPTIME=`date +%b-%d-%y`
-dest=/home/${USER}/wg/backups/w0conf-${BACKUPTIME}.tar
-sudo tar -cf $dest /etc/wireguard/wg0.conf /home/${USER}/wg/clients /home/${USER}/wg/key
+
+
+
+
+
+# Uncomment me after testing and add to the bottom of the deployment script
+# dest=/home/${SrvUser}/wg/backups/w0conf-${BACKUPTIME}.tar
+# sudo tar -cf $dest /etc/wireguard/wg0.conf /home/${SrvUser}/wg/clients /home/${SrvUser}/wg/key
+
+# dest=/home/${SrvUser}/wg/backups/w0conf-${BACKUPTIME}.tar
+# sudo tar -cf $dest /etc/wireguard/wg0.conf /home/${SrvUser}/wg/clients /home/${SrvUser}/wg/key
+
+# crontab -l > wgcron
+# #echo new cron into cron file
+# echo "* * * * * /home/${SrvUser}/wg/backup/backup.sh" >> wgcron
+# #install new cron file
+# crontab wgcron
+# rm wgcron
+
+
+# TESTING SCRIPT
+
+dest=/home/ortusadmin/wg/backups/w0conf-${BACKUPTIME}.tar
+sudo tar -cf $dest /etc/wireguard/wg0.conf /home/ortusadmin/wg/clients /home/ortusadmin/wg/key
+
+dest=/home/ortusadmin/wg/backups/w0conf-${BACKUPTIME}.tar
+sudo tar -cf $dest /etc/wireguard/wg0.conf /home/ortusadmin/wg/clients /home/ortusadmin/wg/key
+
+crontab -l > wgcron
+#echo new cron into cron file
+echo "* * * * * /home/ortusadmin/wg/backup/backup.sh" >> wgcron
+#install new cron file
+crontab wgcron
+rm wgcron
