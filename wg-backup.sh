137468c5298719e98f5052f474840d0270fd4707
Declan Dunne <81355373+RealDeclan@users.noreply.github.com>
2021-05-13 09:43:28 +0100

    Update wg-backup.sh

--- wg-backup.sh
+++ wg-backup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+#SrvUser="ortusadmin"
 SrvUser=$1
 BACKUPTIME=`date +%b-%d-%y`
 dest=/home/${SrvUser}/wg/backup/w0conf-${BACKUPTIME}.tar
-sudo tar -cf $dest /etc/wireguard/wg0.conf /home/${SrvUser}/wg/clients /home/${SrvUser}/wg/key
+sudo tar -cf $dest /etc/wireguard/wg0.conf /home/${SrvUser}/wg/clients /home/${SrvUser}/wg/keys
 
