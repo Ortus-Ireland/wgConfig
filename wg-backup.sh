46659d18dc88aa9335d0cd6d80a8d67a539b6391
Declan Dunne <81355373+RealDeclan@users.noreply.github.com>
2021-07-20 15:58:11 +0100

    Fixed username

--- wg-backup.sh
+++ wg-backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SrvUser="ortusadmin"
-SrvUser=$1
+SrvUser="ortusadmin"
+#SrvUser=$1
 BACKUPTIME=`date +%b-%d-%y`
 dest=/home/${SrvUser}/wg/backup/w0conf-${BACKUPTIME}.tar
 sudo tar -cf $dest /etc/wireguard/wg0.conf /home/${SrvUser}/wg/clients /home/${SrvUser}/wg/keys
